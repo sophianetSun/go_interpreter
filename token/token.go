b3c35a4c66e79b6e222647e24406fc86f0a2d272
swkim <sophianet.sun@gmail.com>
2022-01-21 21:34:11 +0900

    code init

---
+++ token/token.go
@@ -0,0 +1,66 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 식별자 + 리터럴
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// 연산자
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	// 구분자
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// 예약어 키워드
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+var keyword = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIndent(indent string) TokenType {
+	if tok, ok := keyword[indent]; ok {
+		return tok
+	}
+	return IDENT
+}
